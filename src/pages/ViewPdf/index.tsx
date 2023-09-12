b84486eb1dc060f197064d69199772593e821109
Rootedy Batista <rootedybatista@gmail.com>
2023-09-12 16:15:10 -0300

    Removed base64 pdf

--- src/pages/ViewPdf/index.tsx
+++ src/pages/ViewPdf/index.tsx
@@ -1,52 +1,16 @@
-import React, { CSSProperties, useRef, useCallback, useEffect } from 'react';
+import React, { CSSProperties } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Structure } from '../../components/Structure';
-import { pdf } from './pdf64';
 
 function ViewPdf() {
   const navigate = useNavigate();
   const params = useParams();
-  const mfeRef = useRef<any>();
-
-  let fileData = {
-    filename: params.id || '',
-    file: 'https://queksiewkhoon.tripod.com/ontology_01.pdf',
-  }
-
-  const receiver = useCallback((e: any) => {
-    switch (e.data?.event) {
-      case 'mfe-loaded': {
-        /* PDF event parameters in base 64 */
-        const data = {
-          event: 'pdfInBase64',
-          info: {
-            filename: 'Teste',
-            pdfInBase64: pdf,
-          },
-        }
-
-        /* Sending event to mfe from pdf */
-        mfeRef?.current?.contentWindow?.postMessage(data, '*');
-        break;
-      }
-    
-      default:
-        break;
-    } 
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('message', receiver);
-
-    return () => {
-      window.removeEventListener('message', receiver);
-    };
-  }, [receiver]);
 
   /* parameters used to load mfe viewer pdf */
   const queryStringObj = {
     'ffit-key': 'i8Nc7C1gYSMz1OgTU9X1DC6tOholr6e7',
-    ...fileData,
+    filename: params.id || '',
+    file: 'https://queksiewkhoon.tripod.com/ontology_01.pdf',
     buttonGoBackMobile: 'false',
     buttonDownloadMobile: 'true',
     showThumbnails: 'false',
@@ -70,7 +34,6 @@ function ViewPdf() {
         <div style={styles.mfeContainer}>
           <div style={styles.mfeContent}>
             <iframe
-              ref={mfeRef}
               src={`https://mfe.ffit.com.br/mfe/visualizacao-de-pdf?${queryString}`}
               title="Visualizador de PDF"
               style={{ width: '100%', height: '100%' }}
