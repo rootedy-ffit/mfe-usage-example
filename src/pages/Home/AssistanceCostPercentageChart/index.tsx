b86bf6454c54c759b9f4f36fd284e58424f66197
Rootedy Batista <rootedybatista@gmail.com>
2023-09-12 23:45:32 -0300

    Configured step4

--- src/pages/Home/AssistanceCostPercentageChart/index.tsx
+++ src/pages/Home/AssistanceCostPercentageChart/index.tsx
@@ -12,111 +12,14 @@ function AssistanceCostPercentageChart() {
 
   const mfeRef = useRef<HTMLIFrameElement | null>(null);
 
-  const getDataMock = async (newDate?: string, customDetails?: boolean) => {
-    /* This event sends an event to return to the initial state of mfe */
-    mfeRef?.current?.contentWindow?.postMessage({ event: 'handleLoading' }, '*');
-
-    try {
-      let resultLastDate = null;
-      
-      if (!newDate) {
-        resultLastDate = await api.post('/producoes/ultima-data');
-      }
-
-      const resultData = await api.post(
-        '/transparencia/custo-assistencial',
-        { 
-          dataRelatorio: (
-            resultLastDate?.data?.retorno?.dataProducao || newDate
-          )
-        }
-      );
-
-      let infoData = resultData.data;
-
-      if (customDetails && newDate) {
-        /* When changing the date, the return is changed randomly */
-        const rndInt = randomIntFromInterval();
-
-        infoData = {
-          ...infoData,
-          retorno: {
-            ...infoData.retorno,
-            custosAssistenciais: (
-              infoData.retorno.custosAssistenciais || []
-            ).slice(rndInt),
-            mes: Number((newDate || '').split('/')[1]),
-            ano: Number((newDate || '').split('/')[2]),
-          }
-        }
-      }
-
-      const infoLoadData = { event: 'loadData', info: infoData };
-      /* Sending event to mfe from AssistanceCostPercentageChart */
-      mfeRef?.current?.contentWindow?.postMessage(infoLoadData, '*');
-    } catch (error) {
-      alert(`AssistanceCostPercentageChart :: ERROR ==> ${error}`);
-    }
-  }
-
-  const receiver = useCallback((e: any) => {
-    if (e.data.mfeCode === '@ffit/custos-assistenciais') {
-      switch (e.data?.event) {
-        case 'updateSize': {
-          setHeight(e.data.info.height);
-          break;
-        }
-
-        case 'mfe-loaded': {
-          getDataMock();
-          break;
-        }
- 
-        /* When receiving the requestDate event, a modal with the date 
-        picker input is opened */
-        case 'requestDate':
-          handleOpen();
-          break;
-      
-        default:
-          break;
-      }
-    }
-  }, [handleOpen]);
-
-  useEffect(() => {
-    window.addEventListener('message', receiver);
-
-    return () => {
-      window.removeEventListener('message', receiver);
-    };
-  }, [receiver]);
-
-  const queryStringObj = {
-    'ffit-key': 'i8Nc7C1gYSMz1OgTU9X1DC6tOholr6e7',
-    outlined: 'true',
-    title: 'Custos Assistenciais',
-    externalDatePicker: 'true',
-  };
-  
-  const queryString = new URLSearchParams(queryStringObj).toString();
-
   return (
-    <div>
-      <iframe
-        ref={mfeRef}
-        src={`https://mfe.ffit.com.br/mfe/custos-assistenciais?${queryString}`}
-        title="Custos Assistenciais"
-        style={{ width: '100%', height, marginBottom: 20 }}
-        frameBorder="0"
-      />
-      <DatePickerModal
-        // When you close the modal by clicking save, the new date is reloaded
-        onLoadOnClose={async () => {
-          const newDate = inputValue?.set('D', 1).format('DD/MM/yyyy');
-          getDataMock(newDate, true);
-        }}
-      />
+    <div style={{
+      background:'lightgray',
+      width: '100%',
+      height: '100%',
+      minHeight: 360,
+    }}>
+
     </div>
   );
 }
--- src/pages/Home/CollectivePerformanceChart/index.tsx
+++ src/pages/Home/CollectivePerformanceChart/index.tsx
@@ -4,60 +4,16 @@ import api from '../../../services/api';
 function CollectivePerformanceChart() {
   const [height, setHeight] = useState(0);
   const mfeRef = useRef<HTMLIFrameElement | null>(null);
-  
-  const getDataMock = async () => {
-    try {
-      const resultData = await api.post('/bonificacao');
-
-      const data = { event: 'loadData', info: resultData.data }
-
-      mfeRef?.current?.contentWindow?.postMessage(data, '*');
-    } catch (error) {
-      alert(`MonthlyLossRatioChart :: ERROR ==> ${error}`);
-    }
-  }
-
-  const receiver = useCallback((e: any) => {
-    if (e.data.mfeCode === '@ffit/performance-coletiva') {
-      switch (e.data?.event) {
-        case 'mfe-loaded': {
-          getDataMock();
-          break;
-        }
-        case 'updateSize':
-          setHeight(e.data.info.height);
-          break;
-      
-        default:
-          break;
-      } 
-    }
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('message', receiver);
-
-    return () => {
-      window.removeEventListener('message', receiver);
-    };
-  }, [receiver]);
-
-  const queryStringObj = {
-    'ffit-key': 'i8Nc7C1gYSMz1OgTU9X1DC6tOholr6e7',
-    outlined: 'true',
-    title: 'Performance Coletiva',
-  };
-
-  const queryString = new URLSearchParams(queryStringObj).toString();
 
   return (
-    <iframe
-      ref={mfeRef}
-      src={`https://mfe.ffit.com.br/mfe/performance-coletiva?${queryString}`}
-      title="Performance Coletiva"
-      style={{ width: '100%', height }}
-      frameBorder="0"
-    />
+    <div style={{
+      background:'lightgray',
+      width: '100%',
+      height: '100%',
+      minHeight: 360,
+    }}>
+
+    </div>
   );
 }
 
